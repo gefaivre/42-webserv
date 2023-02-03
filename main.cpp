ba243c1c53f169c80b8a1cc51bacbc31b7d74825
gefaivre <gefaivre@student.42.fr>
2023-02-03 11:12:25 +0100

    signal

--- main.cpp
+++ main.cpp
@@ -3,8 +3,16 @@
 #include "engine/CreateResponse.hpp"
 #include "Parser.hpp"
 #include <sys/epoll.h>
+#include <signal.h>
 
 void epolling(vector<Server *> servers);
+
+void sig_handler(int signo)
+{
+	if (signo == SIGINT)
+    std::cout << "SIGINT" << std::endl;
+	exit(0);
+}
 
 int main(int argc, char **argv)
 {
@@ -13,6 +21,8 @@ int main(int argc, char **argv)
 		cerr << "Usage: ./webserv [config file]" << endl;
 		return (1);
 	}
+	  if (signal(SIGINT, sig_handler) == SIG_ERR)
+ 		std::cout << "can't catch SIGINT" << std::endl;
 
 	Parser *config = new Parser(argv[1]);
 
