8e433b7fbca7b0b0152fad4b9b95bdeb7fce4eb2
Josephine BACH <jbach@e2r2p12.clusters.42paris.fr>
2023-02-04 00:38:53 +0100

    index marche, rewrite not ready

--- parsing_conf/Parser.cpp
+++ parsing_conf/Parser.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Parser.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgoncalv <mgoncalv@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jbach <jbach@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 14:58:25 by mgoncalv          #+#    #+#             */
-/*   Updated: 2023/02/03 23:36:52 by mgoncalv         ###   ########.fr       */
+/*   Updated: 2023/02/04 00:30:11 by jbach            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,19 +100,18 @@ void	Parser::parseDirective(size_t nextSemiColon, std::vector<Config *> conf)
 						_repeatedPorts.insert(port);
 					
 				}
-				// else if (ft_starts_with(directive, "rewrite "))
-				// {
-				// 	std::vector<std::string> vector_rew = ft_split(directive.substr(12, directive.length() - 11), ' ');
-				// 	std::cout << "Key:"<<vector_rew[0] << ". Value:"<< vector_rew[1] << std::endl; 
-				// 	exit(1);
-				// }
+				else if (ft_starts_with(directive, "rewrite "))
+				{
+					std::vector<std::string> vector_rew = ft_split(directive.substr(8, directive.length() - 7), ' ');
+					std::cout << "Key:"<<vector_rew[0] << ". Value:"<< vector_rew[1] << std::endl; 
+					// exit(1);
+				}
 				else if (ft_starts_with(directive, "server_name "))
 					conf.back()->setName(ft_split(directive.substr(12, directive.length() - 11), ' '));
 				else if (ft_starts_with(directive, "index "))
 				{
-					conf.back()->setIndex(directive.substr(8, directive.length() - 7));
-					std::cout << "Idx:"<<conf.back()->getIndex()<< "!"<< std::endl;
-					exit(1);
+					conf.back()->setIndex(directive.substr(6, directive.length() - 5));
+					// std::cout << "Idx:"<<conf.back()->getIndex()<< "!"<< std::endl;
 				}
 				else if (ft_starts_with(directive, "autoindex "))
 					parseAutoIndex(directive, conf);
